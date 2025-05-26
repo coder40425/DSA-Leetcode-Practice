ef52216f61db459dddd6802e8622d816362bb0ad
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-05-26 16:52:46 +0530

    Create pascals-traingle-2.cpp

---
+++ ARRAYS & SLIDING WINDOW/pascals-traingle-2.cpp
@@ -0,0 +1,38 @@
+/*  Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+Example 1:
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+Example 2:
+Input: rowIndex = 0
+Output: [1]
+
+Example 3:
+Input: rowIndex = 1
+Output: [1,1] */
+
+// Returns the k-th row of Pascal’s Triangle using O(k) space.
+// Starts with row[0] = 1 and builds each row in-place from right to left.
+// Updates follow Pascal's rule: row[j] += row[j - 1].
+// Reverse loop ensures previous values aren't overwritten during calculation.
+// Time: O(k^2), Space: O(k)
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+    vector<int> row(rowIndex + 1, 0);
+    row[0] = 1;
+
+    for (int i = 1; i <= rowIndex; i++) {
+        for (int j = i; j >= 1; j--) {
+            row[j] += row[j - 1];
+        }
+    }
+    return row;
+}
+};

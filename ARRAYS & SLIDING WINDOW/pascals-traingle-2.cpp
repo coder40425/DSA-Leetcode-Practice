6c629f3c1539c1dcc7b3bc57933996a56dce7449
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-05-26 16:53:40 +0530

    Update pascals-traingle-2.cpp

--- ARRAYS & SLIDING WINDOW/pascals-traingle-2.cpp
+++ ARRAYS & SLIDING WINDOW/pascals-traingle-2.cpp
@@ -2,8 +2,12 @@
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
+               1        
+            1   1
+          1   2    1
+        1   3    3    1
+      1   4   6    4    1
 
- 
 Example 1:
 Input: rowIndex = 3
 Output: [1,3,3,1]

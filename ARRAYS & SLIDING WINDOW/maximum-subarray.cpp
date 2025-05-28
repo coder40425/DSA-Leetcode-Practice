b4b5c2251f1e965c1d70086ec1b7588f01d1eca9
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-05-28 14:46:15 +0530

    Update maximum-subarray.cpp

--- ARRAYS & SLIDING WINDOW/maximum-subarray.cpp
+++ ARRAYS & SLIDING WINDOW/maximum-subarray.cpp
@@ -13,12 +13,24 @@ Explanation: The subarray [1] has the largest sum 1.
 Example 3:
 Input: nums = [5,4,-1,7,8]
 Output: 23
-Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.                               */
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.                               
 
+APPROACH-
+  -We will add one by one to the sum (from i=0 to i<n)
+  -sum = sum + nums[i]
+    maxsum = max(maxsum,sum)
+  -If at any point (sum < 0) means it will not contribute to the maximum subarray, 
+  -So we will make sum = 0 again and move forward in the loop.
+  -In the end, answer will be the maxsum.
+  
+TIME COMPLEXITY - O(N)
+SPACE COMPLEXITY - O(1)                                         */
+
+  
 class Solution {
 public:
   
-  int maxsum=INT_MIN;
+  int maxsum=INT_MIN;     //INT_MIN is the smallest possible value an integer datatype can hold. (used for the first pass)
   int sum=0;
     int maxSubArray(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){

dca7e85bbca4f7cf4dfb0ba470ef29bfd681df0f
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-05-28 15:49:20 +0530

    Update best-time-to-buy-&-sell-stock.cpp

--- ARRAYS & SLIDING WINDOW/best-time-to-buy-&-sell-stock.cpp
+++ ARRAYS & SLIDING WINDOW/best-time-to-buy-&-sell-stock.cpp
@@ -17,11 +17,25 @@ Example 2:
 Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
-                                                                                      */
+
+ APPROACH:
+ -SO BASICALLY IN THIS QST. WE HAVE TO MAXIMISE THE PROFIT BY CHOOSING THE SMALLEST VALUE (TO BUY ON THAT DAY
+  AND LARGEST VALUE (TO SELL ON THAT DAY), BUT SELL INDEX SHOULD COME AFTER BUY INDEX IN THE ARRAY.
+ -SO WE WILL INITIALIZE MIN PRICE & MAX PROFIT.
+ -AND WE WILL GO THROUGH THE ARAY USING LOOP.
+ -WHENEVER PRICE[I] < MIN PRICE, MINPRICE WILL BE EQUAL TO PRICE[I]
+   OTHERWISE (PRICE[I] > MIN PRICE) , MAX PROFIT WILL BE THE MAX OF ( MAX PROIFT , PRICE[I] - MIN PRICE )
+   AS BACUASE PROFIT IS (SELL - BUY)
+ -FINAL ASNWER WILL BE THE MAX PROFIT
+
+   TIME COMPLEXITY- O(N)
+   SPACE COMPLEXITY-O(1)  
+                                         */
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-    int minPrice = INT_MAX;
+    int minPrice = INT_MAX;        //INT_MAX means the largest possible value an integer data type can hold. (used for first pass i.e when i=0)
     int maxProfit = 0;
 
     for (int price : prices) {

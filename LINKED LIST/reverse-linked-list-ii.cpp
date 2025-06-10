036010b3adc70d07324de6a66b668827a0109050
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-06-10 20:06:43 +0530

    Update reverse-linked-list-ii.cpp

--- LINKED LIST/reverse-linked-list-ii.cpp
+++ LINKED LIST/reverse-linked-list-ii.cpp
@@ -8,3 +8,42 @@ Output: [1,4,3,2,5]
 Example 2:
 Input: head = [5], left = 1, right = 1
 Output: [5]                                                                      */
+
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (head == nullptr || left == right) return head;
+
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        // Move curr to the left-th node, prev just before it
+        for (int i = 1; i < left; i++) {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        // Reverse from left to right
+        ListNode* last = prev;          // Node before sublist
+        ListNode* newEnd = curr;        // Will become the end of reversed sublist
+        ListNode* next = nullptr;
+
+        for (int i = 0; i <= right - left; i++) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Reconnect the reversed sublist back
+        if (last != nullptr) {
+            last->next = prev;
+        } else {
+            head = prev;
+        }
+
+        newEnd->next = curr;
+        return head;
+    }
+};

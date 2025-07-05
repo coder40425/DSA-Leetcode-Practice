afd072cdc8ac09fd02b4057442d22a63fc8ab291
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-07-05 16:08:35 +0530

    Update sliding-window-maximum.cpp

--- BINARY SEARCH, HEAPS & PRIORITY QUEUE/sliding-window-maximum.cpp
+++ BINARY SEARCH, HEAPS & PRIORITY QUEUE/sliding-window-maximum.cpp
@@ -1,16 +1,11 @@
-/*The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. 
- So the median is the mean of the two middle values.
+/*You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
+ You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
-For examples, if arr = [2,3,4], the median is 3.
-For examples, if arr = [1,2,3,4], the median is (2 + 3) / 2 = 2.5.
-You are given an integer array nums and an integer k. There is a sliding window of size k which is moving from the very left of the array to the very right. 
-You can only see the k numbers in the window. Each time the sliding window moves right by one position.
-
-Return the median array for each window in the original array. Answers within 10-5 of the actual value will be accepted.
+Return the max sliding window.
 
 Example 1:
 Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
-Output: [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]       */
+Output: [3,3,5,5,6,7]       */
 
 /*
 ##APPROACH 1:-

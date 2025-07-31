3720aad312e8448a1c424fbf7689ff4f787cca1e
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-07-31 20:59:51 +0530

    Create clone-graph.cpp

---
+++ GRAPHS/clone-graph.cpp
@@ -0,0 +1,35 @@
+/*Given a reference of a node in a connected undirected graph.
+Return a deep copy (clone) of the graph.
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+Test case format:
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on.
+The graph is represented in the test case using an adjacency list.
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.      */
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+    Node* cloneGraph(Node* node) {
+        if(!node) return nullptr;
+        return dfs(node);
+    }
+    Node* dfs(Node* node){
+        if(mp.count(node))
+        return mp[node];
+
+        Node* clone=new Node(node->val);
+        mp[node]=clone;
+
+        for(Node* nei: node->neighbors){
+            clone->neighbors.push_back(dfs(nei));
+        }
+        return clone;
+    }
+};
